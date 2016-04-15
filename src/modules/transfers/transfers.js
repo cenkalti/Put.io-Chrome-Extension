a0a83e0cb466891e1f5b64398d9a989a0ffa87f8
Peter Defebvre <macpie59@gmail.com>
2016-04-15 13:24:51 -0700

    Add auto refresh on transfer page + estimated time

--- src/modules/transfers/transfers.js
+++ src/modules/transfers/transfers.js
@@ -1,19 +1,31 @@
 (function() {
     var module = angular.module('transfersModule', ['bytesFilter', 'datesFilter']);
 
-    module.controller('transfersController', ['$scope', '$route', 'putio',
-        function($scope, $route, putio) {
+    module.controller('transfersController', ['$scope', '$route', 'putio', '$interval',
+        function($scope, $route, putio, $interval) {
 
             ga('send', 'pageview', '/transfers');
 
             $scope.loading = true;
             $scope.transfers = [];
-
             $scope.cancel = {};
 
             putio.transfers_list(function(err, data) {
                 $scope.loading = false;
                 $scope.transfers = data.transfers;
+            });
+
+            var reload = $interval(function() {
+                $scope.loading = true;
+                putio.transfers_list(function(err, data) {
+                    $scope.loading = false;
+                    $scope.transfers = data.transfers;
+                });
+            }, 5000);
+
+            $scope.$on('$destroy', function() {
+                $interval.cancel(reload);
+                reload = undefined;
             });
 
             $scope.transfers_clean = function() {
