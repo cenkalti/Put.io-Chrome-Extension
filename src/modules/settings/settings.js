224131b12ebb7de54a50a33f0615f786fdc5ede0
Peter Defebvre <macpie59@gmail.com>
2016-05-05 12:53:31 -0700

    Redo settings skin

--- src/modules/settings/settings.js
+++ src/modules/settings/settings.js
@@ -6,81 +6,10 @@
             var log = new Log(module),
                 message = new Message();
 
-            $scope.home_pages = {
-                home: 'Home',
-                transfers: 'Transfers',
-                files: 'Files',
-                settings: 'Settings',
-                friends: 'Friends',
-                library: 'Library'
-            };
-            $scope.home_page = 'home';
-            $scope.notification = true;
-            $scope.default_folder = {
-                name: 'Loading ...'
-            };
-            $scope.modalOptions = {
-                title: 'Files',
-                saveBtn: {
-                    text: 'Move',
-                    disabled: true
+            $scope.putio = {
+                default_folder: {
+                    name: 'Loading ...'
                 }
-            };
-
-            $scope.notification_text = 'Saved';
-
-            putio.options_get(function(err, options) {
-                $scope.$apply(function() {
-                    if (options.home_page) {
-                        $scope.home_page = options.home_page;
-                    }
-                    if (options.notification !== undefined) {
-                        $scope.notification = options.notification;
-                    }
-                });
-            });
-
-            putio.account_settings(function(err, data) {
-                $scope.default_folder.id = data.settings.default_download_folder;
-
-                putio.file($scope.default_folder.id, function(err, data1) {
-                    $scope.default_folder.name = data1.file.name;
-
-                });
-            });
-
-            $scope.update_home_page = function() {
-                wp.event(module, 'options_update', 'home_page', $scope.home_page);
-
-                putio.options_get(function(err, options) {
-                    options.home_page = $scope.home_page;
-                    putio.options_set(options, function() {
-                        $scope.$apply(function() {
-                            $scope.notify('Default page set to ' + $scope.home_pages[$scope.home_page]);
-                        });
-                    });
-                });
-            };
-
-            $scope.update_notification = function() {
-                wp.event(module, 'options_update', 'notification', $scope.notification.toString());
-
-                putio.options_get(function(err, options) {
-                    options.notification = $scope.notification;
-
-                    putio.options_set(options, function() {
-                        log.debug('sending notification message');
-                        message.send('notification', $scope.notification);
-
-                        $scope.$apply(function() {
-                            if ($scope.notification) {
-                                $scope.notify('Notifications enabled');
-                            } else {
-                                $scope.notify('Notifications disabled');
-                            }
-                        });
-                    });
-                });
             };
 
             $scope.maybe_update_default_folder = function(id) {
@@ -96,8 +25,7 @@
                 putio.account_set_settings({
                     default_download_folder: node.id
                 }, function(err, data) {
-                    $scope.notify('Default folder set to ' + node.name);
-                    $scope.default_folder = {
+                    $scope.putio.default_folder = {
                         name: node.name,
                         id: node.id
                     };
@@ -112,6 +40,41 @@
                     $scope.modalOptions.saveBtn.text = 'Nope Nope Nope';
                     $scope.modalOptions.saveBtn.disabled = true;
                 }
+            };
+
+            $scope.app = {
+                home_pages: {
+                    home: 'Home',
+                    transfers: 'Transfers',
+                    files: 'Files',
+                    settings: 'Settings',
+                    friends: 'Friends',
+                    library: 'Library'
+                },
+                home_page: 'home',
+                notification: true
+            };
+
+            $scope.update_home_page = function() {
+                wp.event(module, 'options_update', 'home_page', $scope.app.home_page);
+
+                putio.options_get(function(err, options) {
+                    options.home_page = $scope.app.home_page;
+                    putio.options_set(options, function() {});
+                });
+            };
+
+            $scope.update_notification = function() {
+                wp.event(module, 'options_update', 'notification', $scope.app.notification.toString());
+
+                putio.options_get(function(err, options) {
+                    options.notification = $scope.app.notification;
+
+                    putio.options_set(options, function() {
+                        log.debug('sending notification message');
+                        message.send('notification', $$scope.app.notification);
+                    });
+                });
             };
 
             $scope.reset_auth = function() {
@@ -135,15 +98,32 @@
                 });
             };
 
-            $scope.notify = function(text) {
-                $scope.notification_text = text;
-                $('.notification')
-                    .hide()
-                    .fadeIn()
-                    .delay(2000)
-                    .fadeOut();
-
+            $scope.treeModalOptions = {
+                title: 'Files',
+                saveBtn: {
+                    text: 'Move',
+                    disabled: true
+                }
             };
+
+            putio.options_get(function(err, options) {
+                $scope.$apply(function() {
+                    if (options.home_page) {
+                        $scope.app.home_page = options.home_page;
+                    }
+                    if (options.notification !== undefined) {
+                        $scope.app.notification = options.notification;
+                    }
+                });
+            });
+
+            putio.account_settings(function(err, data) {
+                $scope.putio.default_folder.id = data.settings.default_download_folder;
+
+                putio.file($scope.putio.default_folder.id, function(err1, data1) {
+                    $scope.putio.default_folder.name = data1.file.name;
+                });
+            });
         }
     ]);
 })();
